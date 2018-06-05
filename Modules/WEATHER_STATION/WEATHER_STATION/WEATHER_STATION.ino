ecce2951b11bb0031f74a4dc712e6d64d391365d
Paulo Vasconcelos <paulobvasconcelos@gmail.com>
2018-06-05 21:51:36 +0100

    [WEATHER_STATION] Merge Sensors
    Code merge in one file (the module's main file).
    WEATHER_STATION.ino should be ready to run with all four sensors

--- Modules/WEATHER_STATION/WEATHER_STATION/WEATHER_STATION.ino
+++ Modules/WEATHER_STATION/WEATHER_STATION/WEATHER_STATION.ino
@@ -19,51 +19,115 @@
  * #define D10 1 // TX0 (Serial console)
  */
 
+# define nSamples 25        // Number of Samples
+
+/*
+ * DHT22
+ */
 # include <DHT.h>
-
-# define nSamples 25   // Number of Samples
-
-# define dataPIN 16    // (D0) DHT22 Data Output Pin
-
-DHT dht(dataPIN, DHT22);
-double humSum = 0;
+# define dhtPIN 16          // (D0) DHT22 Data Pin
+DHT dht(dhtPIN, DHT22);
+int humSum = 0;
 int humValue = 0;
-double tempSum = 0;
+int tempSum = 0;
 int tempValue = 0;
+
+/*
+ * GL5516
+ */
+# define gl5516PIN A0       // (A0) Analog Luminosity Sensor
+int luxSum = 0;
+int luxValue = 0;
+
+/*
+ * BMP280
+ */
+# include <Adafruit_BMP280.h>
+# define bmp280sckPIN  5    // (D1) BMP280 SCK
+# define bmp280misoPIN 4    // (D2) BMP280 MISO
+# define bmp280mosiPIN 0    // (D3) BMP280 MOSI
+# define bmp280csPIN   2    // (D4) BMP280 CS
+Adafruit_BMP280 bmp(bmp280csPIN, bmp280mosiPIN, bmp280misoPIN, bmp280sckPIN);
+int presSum = 0;
+int presValue = 0;
+
+/*
+ * RAIN SENSOR
+ */
+# define rainPIN 14         // (D5) Rain Sensor
+boolean rain = false;
+int rainCount = 0;
 
 /*
  * Custom Functions
  */
-void pinSetup() {
-  pinMode(dataPIN, OUTPUT);
-}
-
-void printToSerial() {
-  Serial.print("-------------------------\n" );
-  // Air Humidity
-  Serial.print("Air Humidity = " );                       
-  Serial.print(humValue);
-  Serial.print("%\n");
-  // Air Temperature
-  Serial.print("Air Temperature = " );                       
-  Serial.print(tempValue);
-  Serial.print(char(176));
-  Serial.print("C\n");
-  Serial.print("-------------------------\n\n" );
-}
-
 void collectSamples() {
   for(int i = 0; i < nSamples; i++) {
-    dht.read(dataPIN);
+    //-------------------------------
+    // DHT22
+    dht.read(dhtPIN);
     humSum += dht.readHumidity();
     tempSum += dht.readTemperature();
+    //-------------------------------
+    // GL5516
+    luxSum += analogRead(gl5516PIN);
+    //-------------------------------
+    // BMP280
+    tempSum += bmp.readTemperature();
+    presSum += bmp.readPressure();
+    //-------------------------------
+    // RAIN SENSOR
+    rainCount++;
+    //-------------------------------
     delay(100);
   }
 }
 
 void calculateValues() {
-  humValue   = humSum  / nSamples;
-  tempValue  = tempSum / nSamples;
+  humValue  = humSum  / nSamples;
+  tempValue = tempSum / nSamples / 2;    // Both DHT22 and BMP280 are reading temperature values
+  luxValue  = luxSum  / nSamples;
+  presValue = presSum / nSamples;
+  rain = rainCount > (nSamples/2);
+}
+
+void resetData() {
+  humSum  = 0;
+  tempSum = 0;
+  luxSum  = 0;
+  presSum = 0;
+  rainCount = 0;
+}
+
+String rainState() {
+  if(rain) { return "DRY"; }
+  return "WET";
+}
+
+void printToSerial() {
+  Serial.print("-------------------------\n" );
+  // Air Humidity
+  Serial.print("Humidity    = ");
+  Serial.print(humValue);
+  Serial.print("%\n");
+  // Air Temperature
+  Serial.print("Temperature = ");
+  Serial.print(tempValue);
+  Serial.print(char(176));
+  Serial.print("C\n");
+  // Luminosity
+  Serial.print("Luminosity  = ");
+  Serial.print(luxValue);
+  Serial.print("%\n");
+  // Air Pressure
+  Serial.print("Pressure    = ");
+  Serial.print(presValue);
+  Serial.print("Pa\n");
+  // Rain
+  Serial.print("Rain Sensor = ");
+  Serial.print(rainState());
+  Serial.print("\n");
+  Serial.print("-------------------------\n\n" );
 }
 
 /*
@@ -72,11 +136,15 @@ void calculateValues() {
 void setup() {
   Serial.begin(9600);
   delay(500);
-  pinSetup();
+  if (!bmp.begin()) {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
 }
 
 void loop() {
   delay(2000);
   collectSamples();
   calculateValues();
+  resetData();
 }
