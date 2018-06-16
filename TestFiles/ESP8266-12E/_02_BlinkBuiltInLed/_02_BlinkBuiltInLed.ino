d7186e17b9020a7fd35c45f3bbe515559345da6e
Paulo Vasconcelos <paulobvasconcelos@gmail.com>
2018-06-16 16:14:23 +0100

    [TestFiles] 02 - Blink Built-In LED
    Program to test connection to built-in LED on NodeMCU (GPIO 2, same as D4)

---
+++ TestFiles/ESP8266-12E/_02_BlinkBuiltInLed/_02_BlinkBuiltInLed.ino
@@ -0,0 +1,37 @@
+/*
+   Paulo Vasconcelos
+   2018 june
+   paulobvasconcelos@gmail.com
+*/
+
+/*
+   ---NodeMCU pinout---
+   #define D0 16
+   #define D1  5 // I2C Bus SCL (clock)
+   #define D2  4 // I2C Bus SDA (data)
+   #define D3  0
+   #define D4  2 // Same as "LED_BUILTIN", but inverted logic
+   #define D5 14 // SPI Bus SCK (clock)
+   #define D6 12 // SPI Bus MISO
+   #define D7 13 // SPI Bus MOSI
+   #define D8 15 // SPI Bus SS (CS)
+   #define D9  3 // RX0 (Serial console)
+   #define D10 1 // TX0 (Serial console)
+*/
+
+# define LEDPin 2
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LEDPin, OUTPUT);
+}
+
+void loop() {
+  // Negative logic (PIN = HIGH => LED = OFF)
+  delay(1000);
+  digitalWrite(LEDPin, HIGH);
+  Serial.print("BuiltIn LED is OFF\n");
+  delay(1000);
+  digitalWrite(LEDPin, LOW);
+  Serial.print("BuiltIn LED is ON\n");
+}
